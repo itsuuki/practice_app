decc2f68ed87841b85ab99b3cd7177f6da917e0d
itsuuki <53384302+itsuuki@users.noreply.github.com>
2021-02-16 23:31:53 +0900

    item_table_branch

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # practice_app
  
+## usersテーブル
+|Column|Type|Options|
+|------|----|-------|
+|name|string|null: false|
+|email|string|null: false|
+|password|integer|null: false|
+
+
+
+### Association
+- has_many items
+
+
+
+
+## itmesテーブル
+|Column|Type|Options|
+|------|----|-------|
+|item_name|string|null: false|
+|item_price|integer|null: false|
+|detail|text|null: false|
+|delivery|text|null: false|
+|quantity|text|null: false|
+
+
+
+|user_id|integer|null: false, foreign_key: true|
+
+
+
+### Association
+- has_many publishers
+- belongs_to :user
+
+
+## Publishersテーブル
+|Column|Type|Options|
+|------|----|-------|
+|publisher_name|string|null: false|
+|item_id|integer|null: false, foreign_key: true|
+
+
+
+### Association
+- belongs_to :item
---
+++ database/migrations/2021_02_16_141053_create_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->text('detail');
+            $table->string('delivery');
+            $table->string('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
